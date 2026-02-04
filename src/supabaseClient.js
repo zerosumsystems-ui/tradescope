293a5c2617dc2d3ff7143d6962a35b7b63bed148
zerosumsystems-ui <zerosumsystems@gmail.com>
2026-02-04 15:32:08 -0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zdjynjmomsaqiptbundx.supabase.co'
-const supabaseAnonKey = 'sb_publishable_jhVzDp3glxiS9W3wmw2LHA_FYtuFwlg'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpkanluam1vbXNhcWlwdGJ1bmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMjM1MDUsImV4cCI6MjA4NTc5OTUwNX0._JBuF7S25A3tZuKgWerihJqsH7zXRtNPhZum5DSZeLc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
